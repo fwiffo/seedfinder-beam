6ca1531d9782c979e7be347025849f6ff52a934f
Tom Schumm <fwiffo@gmail.com>
2018-06-22 01:26:41 -0700

    Register coders for SeedMetadata and Location

--- src/main/java/org/fwiffo/seedfinder/util/Location.java
+++ src/main/java/org/fwiffo/seedfinder/util/Location.java
@@ -1,7 +1,10 @@
 package org.fwiffo.seedfinder.util;
 
 import java.io.*;
+import org.apache.beam.sdk.coders.DefaultCoder;
+import org.apache.beam.sdk.coders.SerializableCoder;
 
+@DefaultCoder(SerializableCoder.class)
 public class Location implements java.io.Serializable {
 	public final int x;
 	public final int z;
--- src/main/java/org/fwiffo/seedfinder/util/SeedMetadata.java
+++ src/main/java/org/fwiffo/seedfinder/util/SeedMetadata.java
@@ -1,7 +1,10 @@
 package org.fwiffo.seedfinder.util;
 
 import java.io.*;
+import org.apache.beam.sdk.coders.DefaultCoder;
+import org.apache.beam.sdk.coders.SerializableCoder;
 
+@DefaultCoder(SerializableCoder.class)
 public class SeedMetadata implements java.io.Serializable {
 	public final long seed;
 	private Location[] huts;
