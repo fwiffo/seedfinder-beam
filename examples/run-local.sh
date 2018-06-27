e96fb544b0576530bf110fe4e798d8de04263484
Tom Schumm <fwiffo@gmail.com>
2018-06-26 19:37:45 -0700

    Fix run-local example

--- examples/run-local.sh
+++ examples/run-local.sh
@@ -2,5 +2,5 @@
 time mvn compile exec:java \
     -Dexec.args=" \
     --output=./output/woodland-mansions-2/seeds \
-    --start_seed=0 --end_seed=1G
+    --start_seed=0 --end_seed=1G \
     --woodland_mansions=2"
